9d965e970dcd89fd7104dc0a9b0afc65ca14ab02
Ujjwal Kumar <ujjwal13818@gmail.com>
2023-11-16 14:46:31 +0530

    added encryption to the user password using salting

--- backend/routes/router.js
+++ backend/routes/router.js
@@ -1,38 +1,53 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-// const schema = require('../models/schemas');
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const Users = require("../models/schemas");
 require("mongoose");
-const Users = require('../models/schemas');
 
-router.post('/sign', async(req ,res) => {
-    const { Name, DOB, Email, Contact, Password } = req.body;
-    
-    const userData = { Name: Name, DOB: DOB, Email: Email, Contact: Contact, Password: Password };
+router.post("/sign", async (req, res) => {
+  const { Name, DOB, Email, Contact, Password } = req.body;
+
+  const salt = await bcrypt.genSalt();
+  const HashedPassword = await bcrypt.hash(Password, salt);
+
+  const userData = {
+    Name: Name,
+    DOB: DOB,
+    Email: Email,
+    Contact: Contact,
+    Password: HashedPassword,
+  };
 
   const newUser = new Users(userData);
 
-    try {
-      await newUser.save();
-      res.status(201).send("User created successfully");
-    } catch (error) {
-      console.error(error);
-      res.status(500).send("Internal Server Error");
-    }
-})
+  try {
+    await newUser.save();
+    res.status(201).send("User created successfully");
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Internal Server Error");
+  }
+});
 
-router.post("/logsig", async(req, res) => {
+router.post("/logsig", async (req, res) => {
   const { Email, Password } = req.body;
   try {
-    const emailFound = await Users.findOne({Email:Email, Password:Password})
-    
-    if (emailFound) {
-      res.json(emailFound);
+    const user = await Users.findOne({ Email: Email });
+    if (user) {
+      const passwordMatched = await bcrypt.compare(Password, user.Password);
+      if (passwordMatched) {
+        res.json(user);
+      }
+      else {
+        res.json("password does not match");
+      }
     }
     else {
-      res.json("not exist");
+      res.json("email does not match");
     }
   } catch (error) {
     res.json("user not found");
   }
-})
+});
 module.exports = router;
